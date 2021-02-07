34d7c45d9f6946bafb7d99d00aee1046381f4a81
Michael Winser <mike.winser@gmail.com>
2021-02-07 15:07:32 -0800

    Added colorpicker and sizepicker

--- index.js
+++ index.js
@@ -13,13 +13,10 @@ const canvas = document.querySelector("#draw");
     let isDrawing = false;
     let lastX = 0;
     let lastY = 0;
-    let hue = 0;
     let direction = true;
 
     function draw(e) {
         if(!isDrawing) return;
-        console.log(e);
-        ctx.strokeStyle = `hsl(${hue}, 100%, 50%`;
         ctx.beginPath();
         //start from
         ctx.moveTo(lastX,lastY);
@@ -29,19 +26,7 @@ const canvas = document.querySelector("#draw");
         ctx.stroke();
         //reset lastX and last Y
         [lastX, lastY] = [e.offsetX, e.offsetY];
-        //increment hue
-        hue++
-        if (hue>360){
-            hue = 0;
-        }
-        if (ctx.lineWidth >=80 || ctx.lineWidth<=1){
-            direction = !direction
-        }
-        if (direction){
-            ctx.lineWidth++
-        }else{
-            ctx.lineWidth--
-        }
+
         
 
     }
@@ -54,6 +39,20 @@ const canvas = document.querySelector("#draw");
     canvas.addEventListener('mousemove',draw)
     canvas.addEventListener('mouseup', ()=> isDrawing = false);
     canvas.addEventListener('mouseout', ()=> isDrawing = false);
+
+const sizePicker = document.getElementById("size")
+sizePicker.addEventListener("input", sizeChange);
+function sizeChange(e){
+    ctx.lineWidth = e.target.value
+}
+
+const colorPicker = document.getElementById("color")
+colorPicker.addEventListener("change", colorChange);
+function colorChange(e){
+    ctx.strokeStyle = e.target.value
+}
+
+
 
 const clearButton = document.getElementsByClassName("clear")[0]
 
